f4f450847d3f3d3deed7b9dae2323f63ca522522
wen09 <wenhaoli1031@gmail.com>
2020-05-03 11:02:46 -0400

    parse domain or ur

--- Snooper_Extension/content.js
+++ Snooper_Extension/content.js
@@ -1,5 +1,16 @@
 window.onload = function(){
-    chrome.storage.sync.set({'value': window.location.href}, function(){
-        alert("The URL of this page is: " + window.location.href);
+    var web = window.location.href;
+    var hostname = (new URL(web)).hostname;
+    var link;
+
+    if (hostname.includes("www.")) {
+        link = hostname.substring(4);
+    }
+    else {
+        link = hostname;
+    }
+
+    chrome.storage.sync.set({'value': link}, function(){
+        alert("The URL of this page is: " + link + " hostname: " + hostname);
     });
 }
